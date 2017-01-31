55c88df0ef265d84746aace329dd1d0039520d31
Jarrad Whitaker <JWhitaker@officeworks.com.au>
2017-01-31 16:31:00 +1100

    initial commit

---
+++ src/queue.ts
@@ -0,0 +1,108 @@
+
+class Queue {
+	
+	private	_workers: WorkToken[]
+	private _waiters: {resolve: (w: WorkToken) => void}[];
+	private _emptyWaiters: {resolve: () => void}[];
+
+	constructor(numWorkers: number ) {
+		this._workers = Array(numWorkers).fill(null).map( () => new WorkToken(this) );
+		this._waiters = [];
+		this._emptyWaiters = [];
+	}
+
+	private _waitForFreeWorker(): Promise<WorkToken> {
+		const freeWorkToken = this._tryGetFreeWorker();
+		if (freeWorkToken) return Promise.resolve(freeWorkToken);
+
+		return new Promise((resolve, _reject) => {
+			this._waiters.push( {resolve} );
+		});
+	}
+
+	private _tryGetFreeWorker(): WorkToken | undefined {
+		const freeWorkToken = this._workers.find( (worker) => !worker.busy);
+		if (! freeWorkToken) return;
+		else {
+			freeWorkToken.lock();
+			return freeWorkToken;
+		}
+	}
+
+	doAsync(f: () => Promise<any> ): Promise<void>  {
+		const waitForWorker = this._waitForFreeWorker();
+		waitForWorker
+		.then( (worker) => {
+			return f()
+			.catch( (e) => {})
+			.then( () => {
+				worker.unlock();
+			});
+		});
+
+		return waitForWorker.then( () => {});
+	}
+
+	do( f: () => any  ): Promise<void>  {
+		const waitForWorker = this._waitForFreeWorker();
+		waitForWorker
+		.then( (worker) => {
+			try {
+				f();
+			} finally {
+				worker.unlock();
+			}
+		});
+
+		return waitForWorker.then( () => {});
+	}
+
+	whenEmpty(): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			this._emptyWaiters.push({resolve});
+		});
+	}
+
+	_checkWorkers() {
+		let waiter;
+		while ( this._waiters.length > 0 ) {
+			const worker = this._tryGetFreeWorker();
+			if (!worker) break;
+
+			const waiter = this._waiters.shift()!;
+			waiter.resolve(worker);
+		}
+
+		if ((this._waiters.length === 0)) {
+			if ( this._workers.find( (w) => w.busy) ) {
+				return;
+			}
+			let emptyWaiter;
+			while ( (emptyWaiter = this._emptyWaiters.shift()) ) {
+				emptyWaiter.resolve();
+			}
+		}
+	}
+}
+
+class WorkToken {
+	
+	busy: boolean;
+	queue: Queue;
+
+	constructor(queue: Queue ) {
+		this.queue = queue;
+	}
+
+	lock() {
+		this.busy = true;
+	}
+
+	unlock() {
+		this.busy = false;
+		this.queue._checkWorkers();
+	}
+}
+
+export = Queue;
+
---
+++ src/test.ts
@@ -0,0 +1,104 @@
+///<reference types="mocha" />
+
+import Queue = require('./queue');
+import assert = require('assert');
+
+function wait(ms: number) {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, ms);
+	});
+}
+
+describe('Queue', () => {
+	it('should execute all synchronous tasks sequentially', () => {
+		const q = new Queue(1);
+
+		const results: number[] = [];
+		for (let i = 0; i < 10; i++) {
+			q.do(() => results.push(i));
+		}
+
+		return q.whenEmpty()
+		.then( () => {
+			assert.deepStrictEqual(results, [0,1,2,3,4,5,6,7,8,9]);
+		});
+	})
+
+	it('should execute all asynchronous tasks sequentially', () => {
+		const q = new Queue(1);
+
+		const timeStart = Date.now();
+
+		const results: number[] = [];
+		for (let i = 0; i < 10; i++) {
+			q.doAsync(() =>
+				wait(5)
+				.then( () => {
+					results.push(i)
+				}
+			));
+		}
+
+
+		return q.whenEmpty()
+		.then( () => {
+			const timeEnd = Date.now();
+			const timeElapsed = timeEnd - timeStart;
+
+			assert(timeElapsed > 5*10, 'too fast to have been sequential');
+			assert.deepStrictEqual(results, [0,1,2,3,4,5,6,7,8,9]);
+		});
+	});
+
+	it('should execute a mix of sync and async tasks sequentially', () => {
+		const q = new Queue(1);
+
+		const results: number[] = [];
+		for (let i = 0; i < 10; i++) {
+			if (i % 2 === 0) {
+				q.do(() => results.push(i));
+			} else {
+				q.doAsync(() =>
+					Promise.resolve()
+					.then( () => {
+						results.push(i)
+					}
+				));
+			}
+
+		}
+
+		return q.whenEmpty()
+		.then( () => {
+			assert.deepStrictEqual(results, [0,1,2,3,4,5,6,7,8,9]);
+		});
+	})
+
+	it('should execute async tasks in parallel', () => {
+		const q = new Queue(4);
+
+		const timeStart = Date.now();
+		const results: number[] = [];
+		for (let i = 0; i < 10; i++) {
+			q.doAsync(() =>
+				wait(5)
+				.then( () => {
+					results.push(i)
+				}
+			));
+		}
+
+		return q.whenEmpty()
+		.then( () => {
+			const timeEnd = Date.now();	
+			const timeElapsed = timeEnd - timeStart;
+
+			// each task takes 5 ms to complete, so if our total time for 10 tasks is much less
+			// than 5 ms * 10 tasks then we can be sure there was some execution happening in
+			// parallel.
+			assert(timeElapsed < 5*10 * 0.5, 'Executed too slow to be sure it was in parallel');
+
+			assert.deepStrictEqual(results.sort(), [0,1,2,3,4,5,6,7,8,9]);
+		});
+	})
+})
